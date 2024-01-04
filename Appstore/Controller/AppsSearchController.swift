93e0fd512cd9ac67e6e15d1361e3d7ba87c5ca21
Turan Çabuk <turan6677@gmail.com>
2024-01-04 18:01:45 +0300

    AppsSearchController has been created.

--- Appstore/Controller/AppsSearchController.swift
+++ Appstore/Controller/AppsSearchController.swift
@@ -7,14 +7,28 @@
 
 import UIKit
 
-class AppsSearchController: UICollectionViewController {
+class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    fileprivate let cellId = "id123"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
     }
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return.init(width: view.frame.width, height: 250)
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 5
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
     init() {
         super.init(collectionViewLayout: UICollectionViewFlowLayout())
     }
---
+++ Appstore/Views/SearchResultCell.swift
@@ -0,0 +1,79 @@
+//
+//  SearchResultCell.swift
+//  Appstore
+//
+//  Created by Turan Çabuk on 4.01.2024.
+//
+
+import UIKit
+
+class SearchResultCell: UICollectionViewCell {
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .red
+        iv.widthAnchor.constraint(equalToConstant: 64).isActive = true
+        iv.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        iv.layer.cornerRadius = 12
+        
+        return iv
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "App Name"
+        return label
+    }()
+    
+    let categoriesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Photos & Video"
+        return label
+    }()
+    
+    let ratingsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "9.26M"
+        return label
+    }()
+    
+    let getButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("GET", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.backgroundColor = .darkGray
+        button.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .yellow
+        
+        let labelsStackView = UIStackView(arrangedSubviews: [
+        nameLabel, categoriesLabel, ratingsLabel
+        ])
+        labelsStackView.axis = .vertical
+        
+        let stackView = UIStackView(arrangedSubviews: [
+        imageView, labelsStackView, getButton
+        ])
+        
+        stackView.spacing = 12
+        stackView.alignment = .center
+        
+        addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

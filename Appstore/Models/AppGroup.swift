bff13722127782c5e02b24f75be7a02e5bd6dd42
Turan Çabuk <turan6677@gmail.com>
2024-01-08 22:57:23 +0300

    AppGroupModel has been created, Service has been reached.

---
+++ Appstore/Models/AppGroup.swift
@@ -0,0 +1,24 @@
+//
+//  AppGroup.swift
+//  Appstore
+//
+//  Created by Turan Çabuk on 8.01.2024.
+//
+
+import Foundation
+
+struct AppGroup: Decodable {
+    
+    let feed: Feed
+}
+
+struct Feed: Decodable {
+    
+    let title: String
+    let results: [FeedResult]
+}
+
+struct FeedResult: Decodable {
+    
+    let name, artistName, artworkUrl100: String
+}

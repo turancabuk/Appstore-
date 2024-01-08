5e3cf41dbac5e38b6a132e6baea9fc7dbe013c72
Turan Çabuk <turan6677@gmail.com>
2024-01-08 20:35:36 +0300

    AppsHeaderHorizontalController, AppsHeaderCell, AppsPageHeader have been created.

---
+++ Appstore/Views/Apps/AppsPageHeader.swift
@@ -0,0 +1,24 @@
+//
+//  AppsPageHeader.swift
+//  Appstore
+//
+//  Created by Turan Çabuk on 8.01.2024.
+//
+
+import UIKit
+
+class AppsPageHeader: UICollectionReusableView {
+    
+    let appHeaderHorizontalController = AppsHeaderHorizontalController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(appHeaderHorizontalController.view)
+        appHeaderHorizontalController.view.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
